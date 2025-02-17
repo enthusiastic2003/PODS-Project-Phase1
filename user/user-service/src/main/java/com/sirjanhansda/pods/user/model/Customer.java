3255b7acd6073bb291edf9a93331d51270aaede7
Your Name <you@example.com>
2025-02-17 16:38:05 +0530

    Refractored User Service

--- user/user-service/src/main/java/com/sirjanhansda/pods/user/model/Customer.java
+++ user/user-service/src/main/java/com/sirjanhansda/pods/user/model/Customer.java
@@ -1,28 +1,22 @@
-package com.sirjanhansda.pods.user.model;
+package com.sirjanhansda.pods.user.model; // Defines the package for this entity class
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;        // Marks this class as a JPA entity (table in the database)
 import jakarta.persistence.Id;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;  // Lombok annotation to generate boilerplate code like toString, equals, hashCode
 
-@Entity
-@Data
-@Getter
-@Setter
+
+/**
+ * Represents a Customer entity in the database.
+ */
+@Entity  // Marks this class as a JPA entity, meaning it corresponds to a database table
+@Data    // Lombok annotation that includes @Getter, @Setter, @ToString, @EqualsAndHashCode
 public class Customer {
 
-    @Id
-    private Integer id;
+    @Id  // Marks this field as the primary key
+    private Integer id; // Unique identifier for the customer
 
-    private String name;
-    private String email;
+    private String name;  // Name of the customer
+    private String email; // Email address of the customer
 
-
-    private Boolean discount_availed = false;
-
-
-
-};
+    private Boolean discount_availed = false; // Flag indicating if the customer has availed a discount
+}
